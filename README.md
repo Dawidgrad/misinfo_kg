28f5e73dbd5ba871d92096c6343eec1996ad8984
Dawid Grad <dawidgrad.work@gmail.com>
2022-05-10 14:20:36 +0100

    Added Ukraine misinfo SVO extraction code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Building a COVID-19 knowledge graph based on the IFCN dataset, and using this approach for misinformation detection
 
 Place the IFCN_COVID19_12748.json dataset in the source/resources directory
+
+Activate the environment: source env/Scripts/activate
+Run the code: python -m source -w "C:\Users\gradd\Documents\software_development\ifcn_covid_kg" -s "C:/Users/gradd/Documents/software_development/shared_libs/stanford-corenlp-4.4.0/*"
--- source/classification.py
+++ source/classification.py
@@ -8,13 +8,101 @@ class Classification:
         self.stanford_path = stanford_path
 
     def run(self):
-        # Retrieve the misinformation data
-        # data = pd.read_json(f'{self.working_dir}\source\\resources\IFCN_COVID19_12748.json')
-        # data = data[['Claim', 'Explaination']]
-        # data = data.reset_index()
-
+        self.ukraine_misinfo()
+        # self.covid_misinfo()
+        return None
+    
+    def ukraine_misinfo(self):
+        # Retrieve the misinformation data        
         data = pd.read_json(f'{self.working_dir}\source\\resources\warDisinfoClaims.json')
-        data = data[['Claim', 'Title']]
+        data = data[['claim', 'title']]
+        data = data.reset_index()
+
+        # Split the covid data into smaller batches so they can be passed to Stanford's package
+        # This is to avoid using too much memory at once
+        filenames = []
+        file_idx = 0
+
+        # Make sure a new file is created to avoid appending to the output from previous run
+        new_filename = f'{self.working_dir}\source\input_files\\claims_{file_idx}.txt'
+        silent_remove(new_filename)
+        file = open(new_filename, 'a', encoding='utf-8')
+
+        # Get claims
+        for idx, row in data.iterrows():
+            claim = row['claim'].strip().replace('\n', ' ')
+            file.write(f'{claim}\n')
+
+            if (idx + 1) % 50 == 0:
+                file.close()
+                file_idx += 1
+                filenames.append(new_filename)
+
+                new_filename = f'{self.working_dir}\source\input_files\\claims_{file_idx}.txt'
+                silent_remove(new_filename)
+
+                file = open(new_filename, 'a', encoding='utf-8')
+        
+        # Create filelist
+        filelist_path = f'{self.working_dir}\source\input_files\\filelist.txt'
+        silent_remove(filelist_path)
+        with open(filelist_path, 'a') as file:
+            for filename in filenames:
+                file.write(f'{filename}\n')
+
+        # Run the Stanford CoreNLP java package over all previously created files
+        args = ['java', '-mx8g', '-cp', self.stanford_path, 'edu.stanford.nlp.naturalli.OpenIE', '-filelist', 
+                f'{self.working_dir}\source\input_files\\filelist.txt', '-output',  
+                f'{self.working_dir}\source\output_files\openie_output_claims.txt', '-tokenize.options', 'untokenizable=noneDelete']
+        args = ' '.join(args)
+
+        self.process = subprocess.Popen(args, shell=True, stderr=subprocess.STDOUT).wait()
+        
+        # Get titles
+        filenames = []
+        file_idx = 0
+
+        # Make sure a new file is created to avoid appending to the output from previous run
+        new_filename = f'{self.working_dir}\source\input_files\\titles_{file_idx}.txt'
+        silent_remove(new_filename)
+        file = open(new_filename, 'a', encoding='utf-8')
+
+        for idx, row in data.iterrows():
+            title = row['title'].strip().replace('\n', ' ')
+            file.write(f'{title}\n')
+
+            if (idx + 1) % 50 == 0:
+                file.close()
+                file_idx += 1
+                filenames.append(new_filename)
+
+                new_filename = f'{self.working_dir}\source\input_files\\titles_{file_idx}.txt'
+                silent_remove(new_filename)
+
+                file = open(new_filename, 'a', encoding='utf-8')
+        
+        # Create filelist
+        filelist_path = f'{self.working_dir}\source\input_files\\filelist.txt'
+        silent_remove(filelist_path)
+        with open(filelist_path, 'a') as file:
+            for filename in filenames:
+                file.write(f'{filename}\n')
+
+        # Run the Stanford CoreNLP java package over all previously created files
+        args = ['java', '-mx8g', '-cp', self.stanford_path, 'edu.stanford.nlp.naturalli.OpenIE', '-filelist', 
+                f'{self.working_dir}\source\input_files\\filelist.txt', '-output',  
+                f'{self.working_dir}\source\output_files\openie_output_titles.txt', '-tokenize.options', 'untokenizable=noneDelete']
+        args = ' '.join(args)
+
+        self.process = subprocess.Popen(args, shell=True, stderr=subprocess.STDOUT).wait()
+        
+        print('Hello there')
+        return None
+
+    def covid_misinfo(self):
+        # Retrieve the misinformation data   
+        data = pd.read_json(f'{self.working_dir}\source\\resources\IFCN_COVID19_12748.json')
+        data = data[['Claim', 'Explaination']]
         data = data.reset_index()
 
         # self.data[['Claim', 'Explaination']].head(50).to_csv(r'C:\Users\gradd\Documents\Software Development\ifcn_covid_kg\source\outputs\ifcn_head.csv')
